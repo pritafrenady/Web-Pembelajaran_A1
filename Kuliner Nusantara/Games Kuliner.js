eb0a8f0fdeb64f38b96b18f0adc8aaae692156f0
pritafrenady <125951305+pritafrenady@users.noreply.github.com>
2023-05-21 20:58:07 +0700

    Add files via upload

---
+++ Kuliner Nusantara/Games Kuliner.js
@@ -0,0 +1,70 @@
+let words = [
+    {
+        word: "rendang",
+        hint: "Makanan Terbaik dalam 50 Makanan Terbaik Dunia"
+    },
+    {
+        word: "nasi goreng",
+        hint: "Nasi goreng khas Indonesia biasanya dimakan dengan acar dan kerupuk"
+    },
+    {
+        word: "sate",
+        hint: "Umumnya menggunakan daging kambing dan ayam"
+    },
+    {
+        word: "soto",
+        hint: "Umumnya dihidangkan dengan rempah-rempah lezat seperti kunyit segar, kayu manis, serai dan daun jeruk"
+    },
+    {
+        word: "gado gado",
+        hint: "Campuran beberapa sayuran yang direbus, kemudian disajikan dengan telur dan saus kacang"
+    },
+    {
+        word: "Pempek",
+        hint: "Terbuat dari campuran tepung dan ikan tenggiri"
+    },
+    {
+        word: "kerak telor",
+        hint: "Jajanan populer Betawi yang disajikan dengan taburan serundeng"
+    },
+    {
+        word: "tahu tek",
+        hint: "Disajikan dengan bumbu petis, isinya terdiri dari tahu goreng dan lotong"
+    },
+    {
+        word: "sate lilit",
+        hint: "Sate khas Bali terbuat dari ikan yang dihaluskan dan dicampur parutan kelapa"
+    },
+    {
+        word: "yogyakarta",
+        hint: "Gudeg makanan khas"
+    },
+    {
+        word: "palembang",
+        hint: "Pempek berasal dari"
+    },
+    {
+        word: "bali",
+        hint: "Pie susu oleh-oleh khas"
+    },
+    {
+        word: "jawa barat",
+        hint: "Surabi berasal dari"
+    },
+    {
+        word: "telor asin",
+        hint: "Oleh-oleh khas Brebes"
+    },
+    {
+        word: "soto mi",
+        hint: "Soto khas kota Bogor"
+    },
+    {
+        word: "papeda",
+        hint: "Makanan khas Maluku"
+    },
+    {
+        word: "coto makassar",
+        hint: "Soto khas Makassar"
+    },    
+]
